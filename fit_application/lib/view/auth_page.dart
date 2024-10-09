2eb51c3ad6b61a803a607647ab271d0cc34e9694
Emanuele Pitoni <emanuele.pitoni.2001@gmail.com>
2024-10-09 22:15:42 +0200

    update

---
+++ fit_application/lib/view/auth_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _Authpage();
+}
+
+class _Authpage extends State<AuthPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+    //throw UnimplementedError();
+  }
+}
